e883bd199b9ec5fb302579272d6cdbaf569badfa
Mohammad Suhel <122422109+Suhel03@users.noreply.github.com>
2023-04-21 17:35:09 +0530

    Add files via upload

---
+++ optimizesCode.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect, useCallback, memo } from 'react';
+import PropTypes from 'prop-types';
+
+const SingleListItem = memo(({ index, isSelected, onClickHandler, text }) => {
+  const handleClick = useCallback(() => {
+    onClickHandler(index);
+  }, [index, onClickHandler]);
+
+  return (
+    <li
+      style={{ backgroundColor: isSelected ? 'green' : 'red'}}
+      onClick={handleClick}
+    >
+      {text}
+    </li>
+  );
+});
+
+SingleListItem.propTypes = {
+  index: PropTypes.number.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+  onClickHandler: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+const List = memo(({ items }) => {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  useEffect(() => {
+    setSelectedIndex(null);
+  }, [items]);
+
+  const handleClick = useCallback((index) => {
+    setSelectedIndex(index);
+  }, []);
+
+  return (
+    <ul style={{ textAlign: 'left' }}>
+      {items?.map((item, index) => (
+        <SingleListItem
+          key={index}
+          onClickHandler={handleClick}
+          text={item.text}
+          index={index}
+          isSelected={selectedIndex === index}
+        />
+      ))}
+    </ul>
+  );
+});
+
+List.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({ text: PropTypes.string.isRequired })
+  ),
+};
+
+export default List;
